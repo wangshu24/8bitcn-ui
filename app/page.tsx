983523e86bf4a2b59054f02391f823e20f25efa3
OrcDev <urosmiric88@gmail.com>
2025-03-27 22:48:49 +0100

    feat(landing page) set padding like shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col p-4 md:p-24 gap-5">
+    <div className="flex flex-col p-4 gap-5 pt-10">
       <h1 className="text-4xl font-bold">Build your retro component library</h1>
       <p className="max-w-2xl">
         A set of 8-bit styled components and a code distribution platform. Works
         with your favorite frameworks. Open Source. Open Code.
       </p>
       <Link href="/components/button" className="w-fit">
-        <Button className="w-fit">Get Started</Button>
+        <Button size="sm" className="w-fit">
+          Get Started
+        </Button>
       </Link>
     </div>
   );
