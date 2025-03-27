5f9a68ee0edd913f5ebcb6472e2dec43e12daffc
OrcDev <urosmiric88@gmail.com>
2025-03-27 22:40:00 +0100

    feat(landing page) add text

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Button from "@/components/ui/8bit/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-4 md:p-24 gap-5 text-center">
-      <h1>8-bitcn/ui</h1>
-      <p>A collection of 8-bit styled components for your next project.</p>
-      <Button>8bitcn/ui</Button>
+    <div className="flex flex-col p-4 md:p-24 gap-5">
+      <h1 className="text-4xl font-bold">Build your retro component library</h1>
+      <p className="max-w-2xl">
+        A set of 8-bit styled components and a code distribution platform. Works
+        with your favorite frameworks. Open Source. Open Code.
+      </p>
+      <Link href="/components/button">
+        <Button className="w-fit">Get Started</Button>
+      </Link>
     </div>
   );
 }
