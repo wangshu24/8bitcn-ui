2558b901fd981dcd72953f81b76d8afd021f1e59
OrcDev <urosmiric88@gmail.com>
2025-04-05 17:35:44 +0200

    refactor(registry): update button path in registry.json to reflect new directory structure

--- components/preview/button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "../ui/8bit/button";
-
-export default function ButtonPreview() {
-  return (
-    <div className="flex flex-col gap-4 p-4 md:p-24">
-      <Button>Click me</Button>
-    </div>
-  );
-}
