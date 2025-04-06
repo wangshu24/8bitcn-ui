e5a7c33a7b163e9744294a2b44bbfe7e3fe9b477
OrcDev <urosmiric88@gmail.com>
2025-04-06 18:54:47 +0200

    refactor(installation-commands): enhance layout and styling of installation commands section

--- app/components/installation-commands.tsx
+++ app/components/installation-commands.tsx
@@ -30,82 +30,88 @@ export default function InstallationCommands({
   };
 
   return (
-    <Tabs defaultValue="pnpm">
-      <TabsList className="bg-[#121212] border-b border-zinc-800 rounded-none h-12 px-2 w-full justify-between">
-        <div>
-          <TabsTrigger
-            value="pnpm"
-            onClick={() =>
-              setCommand(`pnpm dlx shadcn@canary add ${packageUrl}`)
-            }
-            className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-x-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
-          >
-            pnpm
-          </TabsTrigger>
-          <TabsTrigger
-            value="npm"
-            onClick={() => setCommand(`npx shadcn@latest add ${packageUrl}`)}
-            className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
-          >
-            npm
-          </TabsTrigger>
-          <TabsTrigger
-            value="yarn"
-            onClick={() =>
-              setCommand(`yarn dlx shadcn@latest add ${packageUrl}`)
-            }
-            className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
-          >
-            yarn
-          </TabsTrigger>
-          <TabsTrigger
-            value="bun"
-            onClick={() =>
-              setCommand(`bunx --bun shadcn@latest add ${packageUrl}`)
-            }
-            className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
-          >
-            bun
-          </TabsTrigger>
-        </div>
+    <div className="rounded-lg overflow-hidden border border-zinc-800">
+      <div className="bg-[#121212] text-white">
+        <Tabs defaultValue="pnpm">
+          <TabsList className="bg-[#121212] border-b border-zinc-800 rounded-none h-12 px-2 w-full justify-between">
+            <div>
+              <TabsTrigger
+                value="pnpm"
+                onClick={() =>
+                  setCommand(`pnpm dlx shadcn@canary add ${packageUrl}`)
+                }
+                className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-x-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
+              >
+                pnpm
+              </TabsTrigger>
+              <TabsTrigger
+                value="npm"
+                onClick={() =>
+                  setCommand(`npx shadcn@latest add ${packageUrl}`)
+                }
+                className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
+              >
+                npm
+              </TabsTrigger>
+              <TabsTrigger
+                value="yarn"
+                onClick={() =>
+                  setCommand(`yarn dlx shadcn@latest add ${packageUrl}`)
+                }
+                className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
+              >
+                yarn
+              </TabsTrigger>
+              <TabsTrigger
+                value="bun"
+                onClick={() =>
+                  setCommand(`bunx --bun shadcn@latest add ${packageUrl}`)
+                }
+                className="data-[state=active]:bg-transparent data-[state=active]:border-b-2 data-[state=active]:border-b-white data-[state=active]:shadow-none rounded-none px-2 text-white data-[state=inactive]:text-zinc-500"
+              >
+                bun
+              </TabsTrigger>
+            </div>
 
-        <Button
-          variant="ghost"
-          size="icon"
-          className="text-white hover:bg-zinc-800 rounded-md hover:text-white"
-          onClick={handleCopy}
-        >
-          {copied ? (
-            <Check className="size-3" />
-          ) : (
-            <Clipboard className="size-3" />
-          )}
-        </Button>
-      </TabsList>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="text-white hover:bg-zinc-800 rounded-md hover:text-white"
+              onClick={handleCopy}
+            >
+              {copied ? (
+                <Check className="size-3" />
+              ) : (
+                <Clipboard className="size-3" />
+              )}
+            </Button>
+          </TabsList>
 
-      <TabsContent value="pnpm" className="p-4 pt-2">
-        <div className="font-mono text-sm">
-          pnpm dlx shadcn@canary add {packageUrl}
-        </div>
-      </TabsContent>
+          <TabsContent value="pnpm" className="p-4 pt-2">
+            <div className="font-mono text-sm">
+              pnpm dlx shadcn@canary add {packageUrl}
+            </div>
+          </TabsContent>
 
-      <TabsContent value="npm" className="p-4 pt-2">
-        <div className="font-mono text-sm">
-          npx shadcn@latest add {packageUrl}
-        </div>
-      </TabsContent>
+          <TabsContent value="npm" className="p-4 pt-2">
+            <div className="font-mono text-sm">
+              npx shadcn@latest add {packageUrl}
+            </div>
+          </TabsContent>
 
-      <TabsContent value="yarn" className="p-4 pt-2">
-        <div className="font-mono text-sm">
-          yarn dlx shadcn@latest add {packageUrl}
-        </div>
-      </TabsContent>
+          <TabsContent value="yarn" className="p-4 pt-2">
+            <div className="font-mono text-sm">
+              yarn dlx shadcn@latest add {packageUrl}
+            </div>
+          </TabsContent>
 
-      <TabsContent value="bun" className="p-4 pt-2">
-        <div className="font-mono text-sm">
-          bunx --bun shadcn@latest add {packageUrl}
-        </div>
-      </TabsContent>
-    </Tabs>
+          <TabsContent value="bun" className="p-4 pt-2">
+            <div className="font-mono text-sm">
+              bunx --bun shadcn@latest add {packageUrl}
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
   );
 }
