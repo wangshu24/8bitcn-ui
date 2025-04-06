d81f994a8795e19824f5ef3a995a31e4fab10110
OrcDev <urosmiric88@gmail.com>
2025-04-06 19:19:00 +0200

    feat(code-snippet): add CodeSnippet component for copying code to clipboard

---
+++ app/components/code-snippet.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
+import { Check, Clipboard } from "lucide-react";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+
+export default function CodeSnippet({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(children as string);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+
+    toast.success("Copied to clipboard");
+  };
+
+  return (
+    <div className="rounded-lg overflow-hidden border border-zinc-800">
+      <div className="bg-[#121212] text-white flex items-center justify-between px-2 pl-4 py-2">
+        <pre className="text-sm">
+          <code className="text-white">{children}</code>
+        </pre>
+
+        <Button variant="ghost" size="icon" onClick={handleCopy}>
+          {copied ? (
+            <Check className="size-3" />
+          ) : (
+            <Clipboard className="size-3" />
+          )}
+        </Button>
+      </div>
+    </div>
+  );
+}
