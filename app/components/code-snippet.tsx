b01979df954fe5b701417ff0970cf7500609e730
OrcDev <urosmiric88@gmail.com>
2025-04-06 19:24:26 +0200

    fix(code-snippet): adjust layout of code snippet component for improved alignment

--- app/components/code-snippet.tsx
+++ app/components/code-snippet.tsx
@@ -27,7 +27,7 @@ export default function CodeSnippet({
 
   return (
     <div className="rounded-lg overflow-hidden border border-zinc-800">
-      <div className="bg-[#121212] text-white flex items-center justify-between px-2 pl-4 py-2">
+      <div className="bg-[#121212] text-white flex justify-between px-2 pl-4 py-2">
         <pre className="text-sm">
           <code className="text-white">{children}</code>
         </pre>
