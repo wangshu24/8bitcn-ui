9eeb431e180bd57c42b425ad3f890b8bb73de6cc
Aleksandar Lazic <aleksandar.lazic.191277.17@gmail.com>
2025-04-11 09:49:12 +0200

    feat:crate breadcrumb component with proper styles. (#48)
    * feat:crate breadcrumb component with proper styles.
    
    * chore:remove breadcrumb from header

--- config/nav-items.ts
+++ config/nav-items.ts
@@ -1,93 +1,98 @@
 export const navItems = {
-  header: [
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Components",
-      href: "/docs/components/alert",
-    },
-  ],
-  navMain: [
-    {
-      title: "Components",
-      url: "/components/button",
-      items: [
-        {
-          title: "Alert",
-          url: "/docs/components/alert",
-          new: true,
-        },
-        {
-          title: "Avatar",
-          url: "/docs/components/avatar",
-          new: true,
-        },
-        {
-          title: "Badge",
-          url: "/docs/components/badge",
-        },
-        {
-          title: "Button",
-          url: "/docs/components/button",
-        },
-        {
-          title: "Card",
-          url: "/docs/components/card",
-        },
-        {
-          title: "Checkbox",
-          url: "/docs/components/checkbox",
-          new: true,
-        },
-        {
-          title: "Dialog",
-          url: "/docs/components/dialog",
-          new: true,
-        },
-        {
-          title: "Dropdown Menu",
-          url: "/docs/components/dropdown-menu",
-          new: true,
-        },
-        {
-          title: "Input",
-          url: "/docs/components/input",
-        },
-        {
-          title: "Select",
-          url: "/docs/components/select",
-        },
-        {
-          title: "Skeleton",
-          url: "/docs/components/skeleton",
-          new: true,
-        },
-        {
-          title: "Label",
-          url: "/docs/components/label",
-        },
-        {
-          title: "Popover",
-          url: "/docs/components/popover",
-          new: true,
-        },
-        {
-          title: "Switch",
-          url: "/docs/components/switch",
-          new: true,
-        },
-        {
-          title: "Textarea",
-          url: "/docs/components/textarea",
-        },
-        {
-          title: "Tooltip",
-          url: "/docs/components/tooltip",
-          new: true,
-        },
-      ],
-    },
-  ],
+	header: [
+		{
+			label: "Docs",
+			href: "/docs",
+		},
+		{
+			label: "Components",
+			href: "/docs/components/alert",
+		},
+	],
+	navMain: [
+		{
+			title: "Components",
+			url: "/components/button",
+			items: [
+				{
+					title: "Alert",
+					url: "/docs/components/alert",
+					new: true,
+				},
+				{
+					title: "Avatar",
+					url: "/docs/components/avatar",
+					new: true,
+				},
+				{
+					title: "Badge",
+					url: "/docs/components/badge",
+				},
+				{
+					title: "Breadcrumb",
+					url: "/docs/components/breadcrumb",
+					new: true,
+				},
+				{
+					title: "Button",
+					url: "/docs/components/button",
+				},
+				{
+					title: "Card",
+					url: "/docs/components/card",
+				},
+				{
+					title: "Checkbox",
+					url: "/docs/components/checkbox",
+					new: true,
+				},
+				{
+					title: "Dialog",
+					url: "/docs/components/dialog",
+					new: true,
+				},
+				{
+					title: "Dropdown Menu",
+					url: "/docs/components/dropdown-menu",
+					new: true,
+				},
+				{
+					title: "Input",
+					url: "/docs/components/input",
+				},
+				{
+					title: "Select",
+					url: "/docs/components/select",
+				},
+				{
+					title: "Skeleton",
+					url: "/docs/components/skeleton",
+					new: true,
+				},
+				{
+					title: "Label",
+					url: "/docs/components/label",
+				},
+				{
+					title: "Popover",
+					url: "/docs/components/popover",
+					new: true,
+				},
+				{
+					title: "Switch",
+					url: "/docs/components/switch",
+					new: true,
+				},
+				{
+					title: "Textarea",
+					url: "/docs/components/textarea",
+				},
+				{
+					title: "Tooltip",
+					url: "/docs/components/tooltip",
+					new: true,
+				},
+			],
+		},
+	],
 };
