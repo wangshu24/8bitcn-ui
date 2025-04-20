da2355e01a061044a0deffd67ce3f7ccd58c2e76
OrcDev <urosmiric88@gmail.com>
2025-04-20 21:43:45 +0200

    fix(combobox) migrate to new implementation and add examples (#110)

--- components/examples/combobox.tsx
+++ components/examples/combobox.tsx
@@ -1,65 +1,4 @@
-import type { Metadata } from "next"
-
-import { comboBoxMetaData } from "@/lib/metadata"
-import { Separator } from "@/components/ui/separator"
-import { ComboBoxExample } from "@/components/examples/combobox"
-import CodeSnippet from "@/app/docs/components/code-snippet"
-import CopyCommandButton from "@/app/docs/components/copy-command-button"
-import InstallationCommands from "@/app/docs/components/installation-commands"
-import { OpenInV0Button } from "@/app/docs/components/open-in-v0-button"
-
-export const metadata: Metadata = {
-  title: "8bit Combo Box",
-  description:
-    "A combobox component with a searchable dropdown for selecting items from a list.",
-  openGraph: {
-    images: comboBoxMetaData,
-  },
-}
-
-export default function ComboBoxPage() {
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex flex-col md:flex-row items-center justify-between gap-2">
-        <h1 className="text-3xl font-bold">Combo box</h1>
-        <CopyCommandButton
-          copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-combo-box.json`}
-          command={"pnpm dlx shadcn@canary add 8bit-combo-box"}
-        />
-      </div>
-
-      <p className="text-muted-foreground">
-        A combobox component with a searchable dropdown for selecting items from
-        a list.
-      </p>
-
-      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
-        <div className="flex items-center justify-between">
-          <h2 className="text-sm text-muted-foreground sm:pl-3">
-            A simple 8-bit combo box component
-          </h2>
-
-          <div className="flex items-center gap-2">
-            <OpenInV0Button name="8bit-dropdown-menu" className="w-fit" />
-          </div>
-        </div>
-        <div className="flex items-center justify-center min-h-[400px] relative">
-          <ComboBoxExample />
-        </div>
-      </div>
-
-      <h3 className="text-lg font-bold">Installation</h3>
-      <Separator />
-
-      <InstallationCommands
-        packageUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-combo-box.json`}
-      />
-
-      <h3 className="text-lg font-bold mt-10">Usage</h3>
-
-      <Separator />
-
-      <CodeSnippet>{`"use client"
+"use client"
 
 import * as React from "react"
 import { Check, ChevronsUpDown } from "lucide-react"
@@ -151,9 +90,5 @@ export function ComboBoxExample() {
         </Command>
       </PopoverContent>
     </Popover>
-  )
-}
-`}</CodeSnippet>
-    </div>
   )
 }
