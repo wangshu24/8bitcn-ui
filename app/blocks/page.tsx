c394c0b4ec0f014bf0ea2a4879f65657e4aff376
OrcDev <urosmiric88@gmail.com>
2025-04-30 00:00:05 +0200

    Feat/login page block (#150)
    * feat(blocks) add login page
    
    * feat(blocks) add missing border
    
    * feat(registry) add dependencies for the login page block

--- app/blocks/page.tsx
+++ app/blocks/page.tsx
@@ -5,6 +5,7 @@ import { LoginForm } from "@/components/blocks/login-form"
 
 import CopyCommandButton from "../docs/components/copy-command-button"
 import { OpenInV0Button } from "../docs/components/open-in-v0-button"
+import LoginPage from "../login/page"
 
 export const metadata: Metadata = {
   title: "Building Retro Blocks for the Web - 8bitcn/ui",
@@ -13,9 +14,6 @@ export const metadata: Metadata = {
 }
 
 export default function BlocksPage() {
-  const packageUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-login-form.json`
-  const copyCommand = `pnpm dlx shadcn@canary add ${packageUrl}`
-
   return (
     <div className="flex flex-col p-4 gap-5 pt-10">
       <h1 className={`${pressStart.className} md:text-2xl font-bold`}>
@@ -35,13 +33,32 @@ export default function BlocksPage() {
           <div className="flex flex-col md:flex-row items-center gap-2">
             <CopyCommandButton
               command="npx shadcn@latest add 8bit-login-form"
-              copyCommand={copyCommand}
+              copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-login-form.json`}
             />
             <OpenInV0Button name="8bit-login-form" className="w-fit" />
           </div>
         </div>
         <div className="flex items-center justify-center min-h-[400px] relative">
           <LoginForm />
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-2 items-center justify-between">
+        <h2 className="text-sm text-muted-foreground sm:pl-3">
+          A two column login page with a cover image.
+        </h2>
+
+        <div className="flex flex-col md:flex-row items-center gap-2">
+          <CopyCommandButton
+            command="npx shadcn@latest add 8bit-login-page"
+            copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-login-page.json`}
+          />
+          <OpenInV0Button name="8bit-login-page" className="w-fit" />
+        </div>
+      </div>
+      <div className="flex items-center justify-center min-h-[400px] relative border rounded-md">
+        <div className="w-full">
+          <LoginPage />
         </div>
       </div>
     </div>
