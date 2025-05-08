0265dcd289f3c3978aaddfe20d474cc91af57a07
OrcDev <urosmiric88@gmail.com>
2025-05-08 10:40:16 +0200

    feat(registry) add login form 2 (#165)

--- app/blocks/page.tsx
+++ app/blocks/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 
 import { pressStart } from "@/config/fonts"
 import { LoginForm } from "@/components/ui/8bit/blocks/login-form"
+import { LoginForm as LoginForm2 } from "@/components/ui/8bit/blocks/login-form-2"
 
 import CopyCommandButton from "../docs/components/copy-command-button"
 import { OpenInV0Button } from "../docs/components/open-in-v0-button"
@@ -59,6 +60,25 @@ export default function BlocksPage() {
       <div className="flex items-center justify-center min-h-[400px] relative border rounded-md">
         <div className="w-full">
           <LoginPage />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px]">
+        <div className="flex flex-col md:flex-row gap-2 items-center justify-between">
+          <h2 className="text-sm text-muted-foreground sm:pl-3">
+            A simple login form
+          </h2>
+
+          <div className="flex flex-col md:flex-row items-center gap-2">
+            <CopyCommandButton
+              command="npx shadcn@latest add 8bit-login-form"
+              copyCommand={`pnpm dlx shadcn@canary add ${process.env.NEXT_PUBLIC_BASE_URL}/r/8bit-login-form.json`}
+            />
+            <OpenInV0Button name="8bit-login-form" className="w-fit" />
+          </div>
+        </div>
+        <div className="flex items-center justify-center min-h-[400px] relative">
+          <LoginForm2 />
         </div>
       </div>
     </div>
