77e8c9556992244b6169b4fe1aee99a4dee7709a
OrcDev <urosmiric88@gmail.com>
2025-05-13 14:31:55 +0200

    feat(landing-page) update landing page Get Started button to link to accordion docs (#179)

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
         A set of 8-bit styled components and a code distribution platform. Works
         with your favorite frameworks. Open Source. Open Code.
       </p>
-      <Link href="/docs/components/alert" className="w-fit">
+      <Link href="/docs/components/accordion" className="w-fit">
         <Button size="sm" className="w-fit">
           Get Started
         </Button>
