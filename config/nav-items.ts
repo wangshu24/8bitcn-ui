5ae48e8ba0109ab31fa6987e0f41684678db7f8c
OrcDev <urosmiric88@gmail.com>
2025-05-16 12:55:30 +0200

    refactor(nav-items) extract components array and add alphabetical sorting to nav items (#191)

--- config/nav-items.ts
+++ config/nav-items.ts
@@ -1,3 +1,179 @@
+const components = [
+  {
+    title: "Accordion",
+    url: "/docs/components/accordion",
+    new: true,
+  },
+  {
+    title: "Alert",
+    url: "/docs/components/alert",
+  },
+  {
+    title: "Alert Dialog",
+    url: "/docs/components/alert-dialog",
+  },
+  {
+    title: "Avatar",
+    url: "/docs/components/avatar",
+  },
+  {
+    title: "Badge",
+    url: "/docs/components/badge",
+  },
+  {
+    title: "Breadcrumb",
+    url: "/docs/components/breadcrumb",
+  },
+  {
+    title: "Button",
+    url: "/docs/components/button",
+  },
+  {
+    title: "Calendar",
+    url: "/docs/components/calendar",
+  },
+  {
+    title: "Card",
+    url: "/docs/components/card",
+  },
+  {
+    title: "Carousel",
+    url: "/docs/components/carousel",
+  },
+  {
+    title: "Chart",
+    url: "/docs/components/chart",
+  },
+  {
+    title: "Checkbox",
+    url: "/docs/components/checkbox",
+  },
+  {
+    title: "Combo Box",
+    url: "/docs/components/combo-box",
+  },
+  {
+    title: "Command",
+    url: "/docs/components/command",
+  },
+  {
+    title: "Context Menu",
+    url: "/docs/components/context-menu",
+    new: true,
+  },
+  {
+    title: "Collapsible",
+    url: "/docs/components/collapsible",
+  },
+  {
+    title: "Date Picker",
+    url: "/docs/components/date-picker",
+    new: true,
+  },
+  {
+    title: "Dialog",
+    url: "/docs/components/dialog",
+  },
+  {
+    title: "Dropdown Menu",
+    url: "/docs/components/dropdown-menu",
+  },
+  {
+    title: "Hover Card",
+    url: "/docs/components/hover-card",
+  },
+  {
+    title: "Input",
+    url: "/docs/components/input",
+  },
+  {
+    title: "Input OTP",
+    url: "/docs/components/input-otp",
+  },
+  {
+    title: "Scroll Area",
+    url: "/docs/components/scroll-area",
+  },
+  {
+    title: "Select",
+    url: "/docs/components/select",
+  },
+  {
+    title: "Sheet",
+    url: "/docs/components/sheet",
+  },
+  {
+    title: "Sidebar",
+    url: "/docs/components/sidebar",
+    new: true,
+  },
+  {
+    title: "Skeleton",
+    url: "/docs/components/skeleton",
+  },
+  {
+    title: "Slider",
+    url: "/docs/components/slider",
+    new: true,
+  },
+  {
+    title: "Label",
+    url: "/docs/components/label",
+  },
+  {
+    title: "Pagination",
+    url: "/docs/components/pagination",
+  },
+  {
+    title: "Popover",
+    url: "/docs/components/popover",
+  },
+  {
+    title: "Progress",
+    url: "/docs/components/progress",
+  },
+  {
+    title: "Separator",
+    url: "/docs/components/separator",
+  },
+  {
+    title: "Radio Group",
+    url: "/docs/components/radio-group",
+  },
+  {
+    title: "Resizable",
+    url: "/docs/components/resizable",
+  },
+  {
+    title: "Switch",
+    url: "/docs/components/switch",
+  },
+  {
+    title: "Table",
+    url: "/docs/components/table",
+  },
+  {
+    title: "Tabs",
+    url: "/docs/components/tabs",
+  },
+  {
+    title: "Textarea",
+    url: "/docs/components/textarea",
+  },
+  {
+    title: "Toggle",
+    url: "/docs/components/toggle",
+  },
+  {
+    title: "Toggle Group",
+    url: "/docs/components/toggle-group",
+  },
+  {
+    title: "Tooltip",
+    url: "/docs/components/tooltip",
+  },
+]
+
 export const navItems = {
   header: [
     {
@@ -17,181 +193,7 @@ export const navItems = {
     {
       title: "Components",
       url: "/components/accordion",
-      items: [
-        {
-          title: "Accordion",
-          url: "/docs/components/accordion",
-          new: true,
-        },
-        {
-          title: "Alert",
-          url: "/docs/components/alert",
-        },
-        {
-          title: "Alert Dialog",
-          url: "/docs/components/alert-dialog",
-        },
-        {
-          title: "Avatar",
-          url: "/docs/components/avatar",
-        },
-        {
-          title: "Badge",
-          url: "/docs/components/badge",
-        },
-        {
-          title: "Breadcrumb",
-          url: "/docs/components/breadcrumb",
-        },
-        {
-          title: "Button",
-          url: "/docs/components/button",
-        },
-        {
-          title: "Calendar",
-          url: "/docs/components/calendar",
-        },
-        {
-          title: "Card",
-          url: "/docs/components/card",
-        },
-        {
-          title: "Carousel",
-          url: "/docs/components/carousel",
-        },
-        {
-          title: "Chart",
-          url: "/docs/components/chart",
-        },
-        {
-          title: "Checkbox",
-          url: "/docs/components/checkbox",
-        },
-        {
-          title: "Combo Box",
-          url: "/docs/components/combo-box",
-        },
-        {
-          title: "Command",
-          url: "/docs/components/command",
-        },
-        {
-          title: "Context Menu",
-          url: "/docs/components/context-menu",
-          new: true,
-        },
-        {
-          title: "Collapsible",
-          url: "/docs/components/collapsible",
-        },
-        {
-          title: "Date Picker",
-          url: "/docs/components/date-picker",
-          new: true,
-        },
-        {
-          title: "Dialog",
-          url: "/docs/components/dialog",
-        },
-        {
-          title: "Dropdown Menu",
-          url: "/docs/components/dropdown-menu",
-        },
-        {
-          title: "Hover Card",
-          url: "/docs/components/hover-card",
-        },
-        {
-          title: "Input",
-          url: "/docs/components/input",
-        },
-        {
-          title: "Input OTP",
-          url: "/docs/components/input-otp",
-        },
-        {
-          title: "Scroll Area",
-          url: "/docs/components/scroll-area",
-        },
-        {
-          title: "Select",
-          url: "/docs/components/select",
-        },
-        {
-          title: "Sheet",
-          url: "/docs/components/sheet",
-        },
-        {
-          title: "Sidebar",
-          url: "/docs/components/sidebar",
-          new: true,
-        },
-        {
-          title: "Skeleton",
-          url: "/docs/components/skeleton",
-        },
-        {
-          title: "Slider",
-          url: "/docs/components/slider",
-          new: true,
-        },
-        {
-          title: "Label",
-          url: "/docs/components/label",
-        },
-        {
-          title: "Pagination",
-          url: "/docs/components/pagination",
-        },
-        {
-          title: "Popover",
-          url: "/docs/components/popover",
-        },
-        {
-          title: "Progress",
-          url: "/docs/components/progress",
-        },
-        {
-          title: "Separator",
-          url: "/docs/components/separator",
-        },
-        {
-          title: "Radio Group",
-          url: "/docs/components/radio-group",
-        },
-        {
-          title: "Resizable",
-          url: "/docs/components/resizable",
-        },
-        {
-          title: "Switch",
-          url: "/docs/components/switch",
-        },
-        {
-          title: "Table",
-          url: "/docs/components/table",
-        },
-        {
-          title: "Tabs",
-          url: "/docs/components/tabs",
-        },
-        {
-          title: "Textarea",
-          url: "/docs/components/textarea",
-        },
-        {
-          title: "Toggle",
-          url: "/docs/components/toggle",
-        },
-        {
-          title: "Toggle Group",
-          url: "/docs/components/toggle-group",
-        },
-        {
-          title: "Tooltip",
-          url: "/docs/components/tooltip",
-        },
-      ],
+      items: components.sort((a, b) => a.title.localeCompare(b.title)),
     },
   ],
 }
