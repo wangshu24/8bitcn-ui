f276d221603374d31b9934bd838423b9282c0d92
wangshu24 <trandinhphuc1997@gmail.com>
2025-05-18 11:21:49 -0400

    rewrite content component to make use of vaul primitive drawer content and modify drawer handle

--- components/ui/8bit/drawer.tsx
+++ components/ui/8bit/drawer.tsx
@@ -5,7 +5,6 @@ import { cn } from "@/lib/utils"
 import {
   Drawer as ShadcnDrawer,
   DrawerClose as ShadcnDrawerClose,
-  DrawerContent as ShadcnDrawerContent,
   DrawerDescription as ShadcnDrawerDescription,
   DrawerFooter as ShadcnDrawerFooter,
   DrawerHeader as ShadcnDrawerHeader,
@@ -85,12 +84,27 @@ function DrawerContent({
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Content>) {
   return (
-    <ShadcnDrawerContent
-      className={cn("", className, pressStart.className)}
-      {...props}
-    >
-      {children}
-    </ShadcnDrawerContent>
+    <ShadcnDrawerPortal data-slot="drawer-portal">
+      <ShadcnDrawerOverlay />
+      <DrawerPrimitive.Content
+        data-slot="drawer-content"
+        className={cn(
+          "border-t-4 border-foreground dark:border-ring rounded-none",
+          "group/drawer-content bg-background fixed z-50 flex h-auto flex-col",
+          "data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b",
+          "data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t",
+          "data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-l data-[vaul-drawer-direction=right]:sm:max-w-sm",
+          "data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-r data-[vaul-drawer-direction=left]:sm:max-w-sm",
+
+          className,
+          pressStart.className
+        )}
+        {...props}
+      >
+        <div className="bg-muted mx-auto mt-4 hidden h-2 w-[100px] shrink-0 rounded-none group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
+        {children}
+      </DrawerPrimitive.Content>
+    </ShadcnDrawerPortal>
   )
 }
 
