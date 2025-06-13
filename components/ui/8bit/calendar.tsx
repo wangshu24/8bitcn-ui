62f6b358673c70ba53dbee5d3a7151dfd32a4d87
OrcDev <urosmiric88@gmail.com>
2025-06-13 14:23:33 +0200

    chore(calendar) update to new Shadcn calendar (#222)
    * chore(calendar) update to new Shadcn calendar
    
    * refactor(calendar): upgrade icons according to the react day picker v9
    
    * chore(registry) new build

--- components/ui/8bit/calendar.tsx
+++ components/ui/8bit/calendar.tsx
@@ -51,83 +51,88 @@ function Calendar({ className, classNames, font, ...props }: CalendarProps) {
           ...classNames,
         }}
         components={{
-          IconLeft: ({ className, ...props }) => (
-            <svg
-              viewBox="0 0 256 256"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="currentColor"
-              strokeWidth="0.25"
-              color=""
-              className={cn("size-4 shrink-0", className)}
-              aria-label="chevron-left"
-              {...props}
-            >
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 128 136)"
-              ></rect>
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 144 152)"
-              ></rect>
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 160 72)"
-              ></rect>
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 160 168)"
-              ></rect>
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 112 120)"
-              ></rect>
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 128 104)"
-              ></rect>
-              <rect
-                width="14"
-                height="14"
-                rx="1"
-                transform="matrix(-1 0 0 1 144 88)"
-              ></rect>
-            </svg>
-          ),
-          IconRight: ({ className, ...props }) => (
-            <svg
-              viewBox="0 0 256 256"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="currentColor"
-              strokeWidth="0.25"
-              color=""
-              className={cn("size-4 shrink-0", className)}
-              aria-label="chevron-right"
-              {...props}
-            >
-              <rect x="128" y="136" width="14" height="14" rx="1"></rect>
-              <rect x="112" y="152" width="14" height="14" rx="1"></rect>
-              <rect x="96" y="72" width="14" height="14" rx="1"></rect>
-              <rect x="96" y="168" width="14" height="14" rx="1"></rect>
-              <rect x="144" y="120" width="14" height="14" rx="1"></rect>
-              <rect x="128" y="104" width="14" height="14" rx="1"></rect>
-              <rect x="112" y="88" width="14" height="14" rx="1"></rect>
-            </svg>
-          ),
+          Chevron: ({ className, ...props }) => {
+            if (props.orientation === "left") {
+              return (
+                <svg
+                  viewBox="0 0 256 256"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="currentColor"
+                  strokeWidth="0.25"
+                  color=""
+                  className={cn("size-4 shrink-0", className)}
+                  aria-label="chevron-left"
+                  {...props}
+                >
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 128 136)"
+                  ></rect>
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 144 152)"
+                  ></rect>
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 160 72)"
+                  ></rect>
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 160 168)"
+                  ></rect>
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 112 120)"
+                  ></rect>
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 128 104)"
+                  ></rect>
+                  <rect
+                    width="14"
+                    height="14"
+                    rx="1"
+                    transform="matrix(-1 0 0 1 144 88)"
+                  ></rect>
+                </svg>
+              );
+            }
+
+            return (
+              <svg
+                viewBox="0 0 256 256"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="currentColor"
+                strokeWidth="0.25"
+                color=""
+                className={cn("size-4 shrink-0", className)}
+                aria-label="chevron-right"
+                {...props}
+              >
+                <rect x="128" y="136" width="14" height="14" rx="1"></rect>
+                <rect x="112" y="152" width="14" height="14" rx="1"></rect>
+                <rect x="96" y="72" width="14" height="14" rx="1"></rect>
+                <rect x="96" y="168" width="14" height="14" rx="1"></rect>
+                <rect x="144" y="120" width="14" height="14" rx="1"></rect>
+                <rect x="128" y="104" width="14" height="14" rx="1"></rect>
+                <rect x="112" y="88" width="14" height="14" rx="1"></rect>
+              </svg>
+            );
+          },
         }}
         {...props}
       />
